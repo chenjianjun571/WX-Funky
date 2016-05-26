a869f63b23606dc5cc1bde3cf72232a8932c98ab
cjj <chenjianjun571@gmail.com>
2016-05-26 19:01:37 +0800

    提交代码

--- components/config/shot-config.js
+++ components/config/shot-config.js
@@ -5,11 +5,28 @@ import { BaseConfig } from './base'
 import _ from 'lodash'
 
 const ShotConfig  = {
+  // 顶部广告
   'MediaSlider': _.merge({
     'dataUrl': 'vda/index_top',
     'aspectRatio': '3:2',
     'height': 460
-  }, BaseConfig), // 广告轮播
+  }, BaseConfig),
+
+  // 最佳样片热区
+  BestSampleHot: _.merge({
+    'dataUrl': 'sample/weddingshot_index_best_sample?pageIndex=1&pageSize=4', // 数据请求地址
+  },BaseConfig),
+
+  // 最佳客片热区
+  BestPringlesHot: _.merge({
+    'dataUrl': 'pringles/weddingshot_index_best_pringles?pageIndex=1&pageSize=6', // 数据请求地址
+  },BaseConfig),
+
+  // 最新活动
+  NewActivityHot: _.merge({
+    'dataUrl': 'vda/weddingshot_index_activity?pageIndex=1&pageSize=4', // 数据请求地址
+  },BaseConfig)
+
 }
 
 export { ShotConfig }
--- components/shot.jsx
+++ components/shot.jsx
@@ -2,11 +2,222 @@ import React, { PropTypes } from 'react'
 import _ from 'lodash'
 
 import { MediaSlider } from './common/media-slider.jsx'
+import { MediaItem, EmImgProcessType } from './common/media-item.jsx'
+import { ShotConfig } from './config/shot-config'
+
+class NewActivity extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      data:[]
+    };
+  }
+
+  render() {
+    return(
+      <div>
+        <div className="title-box-style-1">
+          <div className="banner">
+            <i></i>
+            <span>幸福可以绽放的如此耀眼</span>
+          </div>
+          <span className="title">最新活动</span>
+        </div>
+
+        <div className="list-box list-suit">
+          <ul className="item-list">
+            {
+              _.map(this.state.data,(v,k)=>{
+                return(
+                  <li key={k} className="item suit-box">
+                    <div className="photo-box">
+                      <MediaItem
+                        aspectRatio="2:1"
+                        imageUrl={v.coverUrlWx || v.coverUrlWeb}
+                        linkUrl={v.linkUrl}
+                        processType={EmImgProcessType.emGD_S_S}
+                        height={200}
+                      />
+                    </div>
+                    <div className="info-box">
+                      <i className="img-title"></i>
+                      <span className="text-title">{v.name}</span>
+                      <span className="text-content">{v.description}</span>
+                    </div>
+                  </li>
+                )
+              })
+            }
+          </ul>
+        </div>
+
+      </div>
+    )
+  }
+
+  componentDidMount() {
+    let url = ShotConfig['NewActivityHot'].baseUrl+ShotConfig['NewActivityHot'].dataUrl;
+    fetch(url)
+      .then(res => {return res.json()})
+      .then(j =>{
+        if(j.success && j.data.length > 0) {
+          this.setState({ data:j.data })
+        }
+      })
+  }
+}
+
+class BestPringles extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      data:[]
+    };
+  }
+
+  render() {
+    return(
+      <div>
+        <a href="/pringles">
+          <div className="title-box-style-1">
+            <div className="banner">
+              <i></i>
+              <span>幸福可以绽放的如此耀眼</span>
+            </div>
+            <span className="title">最佳客片欣赏</span>
+          </div>
+        </a>
+        <div className="list-box list-pringles">
+          <ul className="item-list">
+            {
+              _.map(this.state.data,(v,k)=>{
+                let link = '/pringles/'+v.id
+                return (
+                  <li key={k} className="item">
+                    <MediaItem
+                      aspectRatio="2:3"
+                      imageUrl={v.coverUrlWx || v.coverUrlWeb}
+                      linkUrl={link}
+                      processType={EmImgProcessType.emGD_S_S}
+                      width={400}
+                    />
+                  </li>
+                )
+              })
+            }
+          </ul>
+          <a href="/pringles">
+            <div className="more-button">
+              <span className="icon"></span>
+              <span className="title">更多客片</span>
+            </div>
+          </a>
+        </div>
+
+      </div>
+    )
+  }
+
+  componentDidMount() {
+    let url = ShotConfig['BestPringlesHot'].baseUrl+ShotConfig['BestPringlesHot'].dataUrl;
+    fetch(url)
+      .then(res => {return res.json()})
+      .then(j =>{
+        if(j.success && j.data.length > 0) {
+          this.setState({ data:j.data })
+        }
+      })
+  }
+}
+
+class BestSample extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      data:[]
+    };
+  }
+
+  render () {
+    return (
+      <div>
+        <a href="/sample">
+          <div className="title-box-style-1">
+            <div className="banner">
+              <i></i>
+              <span>幸福可以绽放的如此耀眼</span>
+            </div>
+            <span className="title">最佳样片欣赏</span>
+          </div>
+        </a>
+        <div className="list-box list-sample">
+          <ul className="item-list">
+            {
+              _.map(this.state.data,(v,k)=>{
+                let link = '/sample/'+v.id
+                return (
+                  <li key={k} className="item">
+                    <MediaItem
+                      aspectRatio="2:3"
+                      imageUrl={v.coverUrlWx || v.coverUrlWeb}
+                      linkUrl={link}
+                      processType={EmImgProcessType.emGD_S_S}
+                      width={400}
+                    />
+                  </li>
+                )
+              })
+            }
+          </ul>
+          <a href="/sample">
+            <div className="more-button">
+              <span className="icon"></span>
+              <span className="title">更多样片</span>
+            </div>
+          </a>
+        </div>
+
+      </div>
+    )
+  }
+
+  componentDidMount() {
+    let url = ShotConfig['BestSampleHot'].baseUrl+ShotConfig['BestSampleHot'].dataUrl;
+    fetch(url)
+      .then(res => {return res.json()})
+      .then(j =>{
+        if(j.success && j.data.length > 0) {
+          this.setState({ data:j.data })
+        }
+      })
+  }
+}
 
 class Shot extends React.Component {
   render () {
     return (
-      <div></div>
+      <div className="hssy-home-view">
+        <div className="top-logo-box">
+          <div className="logo-box">
+            <i className="icon-home-logo"></i>
+            <i className="icon-home-word"></i>
+          </div>
+        </div>
+        <div className="adv-header-box">
+          <MediaSlider
+            dataUrl={ShotConfig['MediaSlider'].baseUrl+ShotConfig['MediaSlider'].dataUrl}
+            aspectRatio={ShotConfig['MediaSlider'].aspectRatio}
+            height={ShotConfig['MediaSlider'].height}
+          />
+        </div>
+        <BestSample />
+        <BestPringles />
+        <NewActivity />
+      </div>
     )
   }
 }
